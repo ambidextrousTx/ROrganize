6ddf637cc365a8d9ba65a3920f6f389b27396291
ambidextrousTx <ravisinha@my.unt.edu>
2014-07-21 22:59:21 -0500

    Allow user to enter file patterns

--- UserInterface.scala
+++ UserInterface.scala
@@ -1,4 +1,5 @@
 // Reads the user input
+import java.io.File
 
 class UserInterface {
 	def getDirectoryName(): String = {
@@ -7,7 +8,14 @@ class UserInterface {
     dir
   }
 
+  def getFilePattern(): String = {
+    println("What pattern of files? ")
+    val pattern = readLine()
+    pattern
+  }
+
 }
 
 val ui = new UserInterface
 println(ui.getDirectoryName)
+println(ui.getFilePattern)
